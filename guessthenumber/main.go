c4e9d9280214cb694947bb84748d2e2969d90a79
Rune Remmen <rune.remmen@gmail.com>
2023-05-05 16:57:21 +0200

    checks

--- guessthenumber/main.go
+++ guessthenumber/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	low, high := 0, 100
+	low, high, tries := 0, 100, 0
 
 	fmt.Println("Guess a number between " + strconv.Itoa(low) + " and " + strconv.Itoa(high))
 
@@ -27,13 +27,26 @@ func main() {
 
 		if char == 97 {
 			low = guess + 1
+			tries++
+			if high == guess {
+				fmt.Println("It cant be higher then", high)
+				fmt.Println("it took " + strconv.Itoa(tries) + " tries!")
+				break
+			}
 		} else if char == 98 {
 			high = guess - 1
+			tries++
+			if low == guess {
+				fmt.Println("It cant be lower then", low)
+				fmt.Println("it took " + strconv.Itoa(tries) + " tries!")
+				break
+			}
 		} else if char == 99 {
 			fmt.Println("Winner winner, chicken dinner!")
+			fmt.Println("it took " + strconv.Itoa(tries) + " tries!")
 			break
 		} else {
-			fmt.Println("try again")
+			fmt.Println("Invalid choice, try again.")
 		}
 	}
 }
